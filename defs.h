43e7445220b44e261eaaf5db540f16b3b4b2d115
Arman <121860974+RizzZero@users.noreply.github.com>
2024-12-06 17:53:36 +0330

    Update defs.h

--- defs.h
+++ defs.h
@@ -155,6 +155,7 @@ int             argstr(int, char**);
 int             fetchint(uint, int*);
 int             fetchstr(uint, char**);
 void            syscall(void);
+int             set_proc_sched(int,int,int);
 
 // timer.c
 void            timerinit(void);
